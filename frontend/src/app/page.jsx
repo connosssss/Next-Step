caff40544cc378d1a508a163f08ea71eb5ae207f
connosssss <connorgriffin2424@gmail.com>
2025-09-03 12:43:52 -0700

    front end updates

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -49,28 +49,44 @@ export default function Home() {
   };
 
   return (
-    <div className="w-screen h-screen bg-emerald-400 text-black">
-      
-    
+    <div className="w-screen h-screen bg-emerald-300 text-emerald-700">
+      <div className='p-12'>
+      <h1 className='text-5xl font-bold '>Next Step</h1>
+      <p className='text-xl py-6 w-1/3 font-semibold'> A stock market predictor that uses sckit to make linear regression and 
+        random forest models, combining the results to get the most accurate prediction </p>
+      <div className='w-full flex justify-start mb-4 gap-5 items-center'>
+
+        <textarea className='bg-emerald-100/70 h-12 rounded-md items-center text-xl resize-none text-center '
+        placeholder='AAPL'
+        onChange={e => setTicker(e.target.value)}
+        cols={9}/>
+
+        
         <button
           onClick={handlePredict}
           disabled={loading}
-          className="bg-red-200 p-2"
+          className="bg-emerald-100/70 px-4 py-2 text-2xl font-bold rounded-xl"
         >
           {loading ? 'Loading...' : 'Predict'}
         </button>
+        {loading && (
+          <div className=' w-full  flex items-center '>
+            <p> The models train after in real time, may take a while to load</p>
+            </div>
+        )}
 
       {error && (
-        <div className="bg-red-600">
-          <div>Error:</div> {error}
-        </div>
-      )}
+          <div className="bg-red-600">
+            <div>Error:</div> {error}
+          </div>
+        )}
 
-      
-      <div className="bg-white p-4 rounded">
-        <div className="text-xs overflow-auto">{JSON.stringify(stockData)}</div>
       </div>
+        <div className="bg-white p-4 rounded">
+          <div className="text-xs overflow-auto">{JSON.stringify(stockData)}</div>
+        </div>
       
+      </div>
     </div>
   );
 }
